d0a5add3fef60d348e531ba2003fecba1da129ac
dlukowicz <lukowicz@student.wszib.edu.pl>
2021-03-09 19:39:08 +0100

    Zmiany

--- src/pl/edu/wszib/HelloWorld.java
+++ src/pl/edu/wszib/HelloWorld.java
@@ -1,7 +1,12 @@
 package pl.edu.wszib;
 
 public class HelloWorld {
+    //zmiany
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 0; i<15; ++i)
+        {
+            System.out.println("Hello World! DLukowicz");
+        }
+
     }
 }
